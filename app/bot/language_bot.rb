218e6d080864895a8efd1853ffec9a3834eb9cac
Manh T. Truong <manhattan.vn@gmail.com>
2016-11-15 20:14:15 +0700

    Milestone 4 ongoing

--- app/bot/language_bot.rb
+++ app/bot/language_bot.rb
@@ -1,8 +1,22 @@
 class LanguageBot < SlackRubyBot::Bot
   # leave it empty for now
    command 'hello' do |client, data, match|
-     I18n.locale = :vi
+
+     user = User.find_or_create_by(username: data.user)
+#     binding.pry 
+     I18n.locale = user.locale
      text = I18n.t "hello", user: data.user
      client.say(text: text, channel: data.channel)
    end
+
+   command "language" do |client, data, match|
+     user = User.find_or_create_by(username: data.user)
+     if match[:expression]
+        user.locale = match[:expression]
+        user.save!
+     end
+     text = "Change language to #{user.locale} for <@%{user}>"
+     client.say(text: text, channel: data.channel)
+   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161115130556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "flash_cards", force: :cascade do |t|
+    t.string   "term"
+    t.string   "definition"
+    t.string   "image"
+    t.integer  "rank"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "locale",     default: "en"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
 
 end
